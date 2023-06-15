6ed5ab31fa5682df2ef415247809e20f78e9b396
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-06-15 20:25:04 +0200

    [Module 3] Task 2 Solution 3

--- Module3/Exercise2/ConsentService.cs
+++ Module3/Exercise2/ConsentService.cs
@@ -6,7 +6,7 @@
 
         public void Add(ConsentDto item)
         {
-            _list.Add(new () { Id = item.Id, Name = item.Name });
+            _list.Add(item.AsEntity());
         }
 
         public ConsentDto? Get(int id)
@@ -15,11 +15,7 @@
             {
                 if (item.Id == id)
                 {
-                    return new ()
-                    {
-                        Id = item.Id,
-                        Name = item.Name
-                    };
+                    return item.AsDto();
                 }
             }
 
---
+++ Module3/Exercise2/Extensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Exercise2
+{
+    public static class Extensions
+    {
+        public static Headphones AsEntity(this HeadphonesDto dto)
+        {
+            return new Headphones
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+            };
+        }
+
+        public static HeadphonesDto AsDto(this Headphones entity)
+        {
+            return new HeadphonesDto
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+            };
+        }
+
+        public static Consent AsEntity(this ConsentDto dto)
+        {
+            return new Consent
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+            };
+        }
+
+        public static ConsentDto AsDto(this Consent entity)
+        {
+            return new ConsentDto
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+            };
+        }
+    }
+}
