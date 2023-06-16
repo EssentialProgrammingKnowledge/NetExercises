78e99d7d2e65b61bd8db8a6f4868c596aff50d11
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-06-16 22:19:18 +0200

    [Module 3] Task 4

---
+++ Module3/Exercise4/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise4
+{
+    public class Client
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine($"Message Sent! {message}");
+        }
+    }
+}
