404dbbaecd4abf8217863a6ca88476e3ff7b73e6
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-06-16 21:55:06 +0200

    [Module 3] Task 3 Solution 1

--- Module3/Exercise3/DbService.cs
+++ Module3/Exercise3/DbService.cs
@@ -1,7 +1,14 @@
 ﻿namespace Exercise3
 {
-    public class DbService : Service
+    public class DbService
     {
+        private readonly DbConnection _dbConnection;
+
+        public DbService(DbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
         public void Send<T>(T obj) where T : class
         {
             _dbConnection.Send(obj);
--- Module3/Exercise3/FtpService.cs
+++ Module3/Exercise3/FtpService.cs
@@ -1,7 +1,14 @@
 ﻿namespace Exercise3
 {
-    public class FtpService : Service
+    public class FtpService
     {
+        private readonly FtpConnection _ftpConnection;
+
+        public FtpService(FtpConnection ftpConnection)
+        {
+            _ftpConnection = ftpConnection;
+        }
+
         public void Send<T>(T obj) where T : class
         {
             _ftpConnection.Send(obj);
