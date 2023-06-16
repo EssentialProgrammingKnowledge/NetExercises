7f7b42a2083e535a1e0c22482ff101a7cedb8ab5
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-06-16 21:59:10 +0200

    [Module 3] Task 3 Solution 2

--- Module3/Exercise3/MessageHandler.cs
+++ Module3/Exercise3/MessageHandler.cs
@@ -2,9 +2,14 @@
 {
     public class MessageHandler
     {
+        public Action<string> OnSendMessage;
+        public event EventHandler<string> OnSendMessageEventHandler;
+
         public void SendMessage(string message)
         {
             Console.WriteLine($"Message Send {message}");
+            OnSendMessage(message);
+            OnSendMessageEventHandler.Invoke(this, message);
         }
     }
 }
--- Module3/Exercise3/Program.cs
+++ Module3/Exercise3/Program.cs
@@ -4,6 +4,8 @@ using Exercise3;
 Console.WriteLine("Hello, World!");
 
 var messageHandler = new MessageHandler();
+messageHandler.OnSendMessage += (msg) => Console.WriteLine($"OnSendMessage {msg}");
+messageHandler.OnSendMessageEventHandler += (sender, eventArgs) => Console.WriteLine($"OnSendMessageEventHandler {sender} {eventArgs}");
 messageHandler.SendMessage("Test1234");
 messageHandler.SendMessage("Test12345");
 messageHandler.SendMessage("Test123456");
